d7401cb292b642c8246c127f3d203e41d5a72be6
JosiahWI <josiah_vanderzee@mediacombb.net>
2019-10-29 14:26:18 -0500

    Make method names snake case, and use List type annotation where necessary.

--- src/items.py
+++ src/items.py
@@ -6,9 +6,9 @@ class ItemInterface:
 
 
     # Items is a list of item dictionaries.
-    def __init__(self, items: [ Dict[str, int] ]) -> None:
+    def __init__(self, items: List[ Dict[str, int] ]) -> None:
         self.items = items
-        self.item_string = self.getItemString()
+        self.item_string = self.get_item_string()
 
 
     # See getItemString
@@ -18,38 +18,38 @@ class ItemInterface:
 
     # Get an itemString, which may not be an exact match,
     # an return the item's ID if it exists.
-    def lookupID(self, item_string: str) -> int:
+    def lookup_id(self, item_string: str) -> int:
         pass
 
 
     # Get an item's name from its ID.
-    def getName(self, item_id: int) -> str:
+    def get_name(self, item_id: int) -> str:
         pass
 
 
     # Get an item's cost from its ID.
-    def getCost(self, item_id: int) -> int:
+    def get_cost(self, item_id: int) -> int:
         pass
 
 
     # Get an item's boost from its ID.
-    def getBoost(self, item_id: int) -> int:
+    def get_boost(self, item_id: int) -> int:
         pass
 
 
     # Get the default quantity of an item from its ID.
-    def getDefaultQty(self, item_id: int) -> int:
+    def get_default_qty(self, item_id: int) -> int:
         pass
 
 
     # Key should be a function that accepts an Item and returns
     # True or False. filterBy will return a list of all items for
     # which key returns True.
-    def filterBy(self, key: Callable) -> [int]:
+    def filter_by(self, key: Callable) -> List[int]:
         pass
 
 
     # Return a human readable string. This should similar to
     # the string format already used by coingames.
-    def getItemString(self) -> str:
+    def get_item_string(self) -> str:
         pass
