ab6b066245fc5a8283a696f8b71cad2bc7c421b7
luk3yx <luk3yx@users.noreply.github.com>
2019-10-31 17:49:32 +1300

    Still broken

--- procoin_bot.py
+++ procoin_bot.py
@@ -1,5 +1,6 @@
-import discord
-from discord import commands
+import discord # type: ignore
+from discord.ext import commands # type: ignore
+import os
 
 bot = commands.Bot(command_prefix='&')
 bot.load_extension('procoin_cog')
@@ -7,5 +8,12 @@ bot.load_extension('procoin_cog')
 @bot.event
 async def on_ready():
     print('Logged in')
-    
+
 # TODO implement discord error handling
+
+# Store the token in token.txt, git should ignore it.
+token_file = os.path.join(os.path.dirname(__file__), 'token.txt')
+if __name__ == '__main__':
+    with open(token_file, 'r') as f:
+        token = f.read().strip()
+    bot.run(token)
--- procoin_cog.py
+++ procoin_cog.py
@@ -1,4 +1,4 @@
-from discord.ext import commands, tasks
+from discord.ext import commands, tasks # type: ignore
 from typing import Any, Tuple
 
 # Local imports
@@ -12,26 +12,29 @@ class BotInterface(commands.Cog, ProCoin):
 
     @commands.command
     # both arguments need type annotations
-    async def purchase(self, ctx, *parameters) -> None:
-        
+    async def purchase(self, ctx, *parameters: str) -> None:
+
         if len(parameters) < 2:
             await ctx.send('Not enough parameters to purchase <item> <qty>.')
-            
+
         user_id = ctx.author.id
         item_string = ''.join(parameters[:-1])
         try:
-          qty = int(parameters[-1])
+            qty = int(parameters[-1])
         except ValueError:
-          await ctx.send('Quantity must be an integer.')
-        
+            await ctx.send('Quantity must be an integer.')
+
         transaction_result = self.buy(user_id, item_string, qty)
         if not transaction_result:
-          await ctx.send('Transaction failed. We need some actual exceptions'\
-                         ' instead of just True or False.')
-                         
+            await ctx.send('Transaction failed. We need some actual ' \
+                         'exceptions instead of just True or False (agreed).')
+
         await ctx.send(f'<@{user_id}> bought {qty}'\
                        f' {self.items.lookup(item_string)}')
-                       
+
     @tasks.loop(minutes=60.0)
-    def _update_store(self) -> None:
+    async def _update_store(self) -> None:
         self._generate_store()
+
+def setup(bot):
+    bot.add_cog(BotInterface())
